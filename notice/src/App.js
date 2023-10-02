728c8a6a095afc140fc9dc46b243271e02baaad2
lucas-hm <80957998+lucas-hm@users.noreply.github.com>
2023-10-02 12:10:00 -0300

    Terminado

--- notice/src/App.js
+++ notice/src/App.js
@@ -14,23 +14,23 @@ function App() {
         <button id="close-image"><img src="truco.png" alt="Truco" width="100" height="100"></img> El jugador a la derecha del dador es el “mano” y cuenta con ciertas preferencias en el juego, además de ser el primero en jugar. El último jugador de cada bando, es el “pie”, que suele actuar como capitán y estratega del equipo. Cada jugador estudia las posibilidades que ofrecen sus cartas y, si tiene flor o buen envido, lo comunicará por señas a su compañero o compañeros, al tiempo que observa las de los otros jugadores. Empieza a jugar el “mano”, que puede cantar flor si la tiene, apostar a envido o jugar una de sus cartas. independientemente de lo que haga, cada jugador en su turno tiene esas tres opciones hasta que se juega una carta, ya que en este caso se empieza a jugar el truco y ya no se admite que se cante flor o envido, aunque sí responder a esos cantos. Resueltas las apuestas, se juegan las cartas para conocer al ganador del truco. Tras finalizar éste, se procede a la anotación del juego parcial. Durante el juego, se dice que un jugador o un bando va a la pesca cuando guarda silencio a la espera de que los contrarios sean los primeros en apostar o trucar para entonces responderles. Orden y valor de las cartas
 El orden de las cartas es válido para el truco y determina el ganador de la baza. El valor de las cartas lo es para las apuestas sobre flor y envido.
 
-En el Truco hay cuatro cartas especiales que se conocen como cartas bravas y son las superiores de toda la baraja. El orden de estas cartas, de mayor a menor, es el siguiente: as de espadas (espadilla), as de bastos (bastillo), siete de espadas y siete de oros (sietes bravos).
+En el Truco hay cuatro cartas especiales que se conocen como cartas bravas y son las superiores de toda la baraja. El orden de estas cartas, de mayor a menor, es el siguiente: as de espadas (espadilla), as de bastos (basto), siete de espadas y siete de oros (sietes bravos).
 
 Para el resto, el orden, de mayor a menor, es: treses, doses, ases falsos (as de oros y de copas), cartas negras (reyes, caballos y sotas), sietes falsos (siete de copas y de bastos), seises, cincos y cuatros.
 
 El valor de las cartas, para la flor y el envido, es el siguiente: las cartas negras valen 0 y las restantes tienen el valor en tantos que señala su índice.</button>
         
-        <button id="close-image"><img src="RDR2.png" alt="Red Dead Redemption 2" width="100" height="100"></img>Red Dead Redemption</button>
+        <button id="close-image"><img src="RDR2.png" alt="Red Dead Redemption 2" width="500" height="500"></img>Los territorios estadounidenses que, durante parte de los siglos XVIII y XIX, tuvieron un gran flujo migratorio (gracias a la fiebre del oro) y  son numerosa las historias, tanto en la literatura como en la ficción cinematográfica, que relatan episodios de la vida en aquella zona, los vaqueros, pistoleros y forajidos que por allí frecuentaban e incluso los nativos que habitaban (popularmente conocidos durante muchos años como indios). Tras finalizar la Guerra de Independencia, las denominadas hasta entonces como "Trece Colonias británicas en Norteamérica" pasaron a formar un nuevo país conocido como Estados Unidos de América y el río Misisipí, cuyo caudal va de norte a sur, pasó a ser una frontera física y las tierras que se encontraban al oeste del mencionado río se convirtieron en objetivo prioritario para anexionarlos a los Estados Unidos.</button>
 
-        <button id="close-image"><img src="GTA5.png" alt="GTA5" width="100" height="100"></img>GTA 5</button>
+        <button id="close-image"><img src="GTA5.png" alt="GTA5" width="500" height="500"></img>GTA 5</button>
 
-        <button id="close-image"><img src="Fifa.png" alt="Fifa" width="100" height="100"></img> Fifa</button>
+        <button id="close-image"><img src="Fifa.png" alt="Fifa" width="500" height="500"></img> Fifa</button>
 
-        <button id="close-image"><img src="NBA2K23.png" alt="NBA 2K 23" width="100" height="100"></img> NBA 2K 23</button>
+        <button id="close-image"><img src="NBA2K23.png" alt="NBA 2K 23" width="500" height="500"></img> NBA 2K 23</button>
 
-        <button id="close-image"><img src="NFS.png" alt="Need For Speed" width="100" height="100"></img> Need For Speed</button>
+        <button id="close-image"><img src="NFS.png" alt="Need For Speed" width="500" height="500"></img> Need For Speed</button>
 
-        <button id="close-image"><img src="bloodborne.png" alt="Dark souls bloodborne" width="100" height="100"></img>Bloodborne</button>
+        <button id="close-image"><img src="bloodborne.png" alt="Dark souls bloodborne" width="500" height="500"></img>Bloodborne</button>
       </header>
     </div>
   );
