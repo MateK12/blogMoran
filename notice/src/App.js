1ce97ec64cf5cdf2d71c3deb98ca12ecaaa17e6e
lucas-hm <lucashugomoran12@gmail.com>
2023-10-14 12:58:18 -0300

    terminado

--- notice/src/App.js
+++ notice/src/App.js
@@ -22,15 +22,40 @@ El valor de las cartas, para la flor y el envido, es el siguiente: las cartas ne
         
         <button id="close-image"><img src="RDR2.png" alt="Red Dead Redemption 2" width="500" height="500"></img>Los territorios estadounidenses que, durante parte de los siglos XVIII y XIX, tuvieron un gran flujo migratorio (gracias a la fiebre del oro) y  son numerosa las historias, tanto en la literatura como en la ficción cinematográfica, que relatan episodios de la vida en aquella zona, los vaqueros, pistoleros y forajidos que por allí frecuentaban e incluso los nativos que habitaban (popularmente conocidos durante muchos años como indios). Tras finalizar la Guerra de Independencia, las denominadas hasta entonces como "Trece Colonias británicas en Norteamérica" pasaron a formar un nuevo país conocido como Estados Unidos de América y el río Misisipí, cuyo caudal va de norte a sur, pasó a ser una frontera física y las tierras que se encontraban al oeste del mencionado río se convirtieron en objetivo prioritario para anexionarlos a los Estados Unidos.</button>
 
-        <button id="close-image"><img src="GTA5.png" alt="GTA5" width="500" height="500"></img>GTA 5</button>
+        <button id="close-image"><img src="GTA5.png" alt="GTA5" width="500" height="500"></img>Para el área formativa, este juego ofrece los variados conflictos que pueden estar presentes y que invitan a los jóvenes a reflexionar sobre el poder de la mentira, el sentido de la violencia y la real necesidad de la comunidad para poder desarrollarse.</button>
 
         <button id="close-image"><img src="Fifa.png" alt="Fifa" width="500" height="500"></img> Fifa</button>
 
-        <button id="close-image"><img src="NBA2K23.png" alt="NBA 2K 23" width="500" height="500"></img> NBA 2K 23</button>
+        <button id="close-image"><img src="NBA2K23.png" alt="NBA 2K 23" width="500" height="500"></img>El baloncesto no es un deporte fácil: se necesita resistencia, espíritu de equipo y grandes habilidades para superar a los oponentes y ganar el juego. El baloncesto es mucho más que una cancha, una pelota y un árbitro. Aquí hay algunos datos interesantes que probablemente no conozcas sobre este deporte.
+1. James Naismith inventó el baloncesto
+En 1891 su jefe de aquel entonces le pide que invente un deporte bajo techo, atribuyéndole la creación del deporte a James Naismith. Profesor de educación física, también escribió el libro original de reglas del baloncesto y fundó el programa de baloncesto de la Universidad de Kansas.
+2. El baloncesto se jugaba con una pelota diferente
+Por extraño que parezca, el baloncesto se jugaba originalmente con una pelota de fútbol y canastas de durazno.
+3. No estaba permitido el driblar
+Los jugadores nunca podían hacer avanzar la pelota. En cambio, cada jugador tenía que tirarlo desde donde lo atrapara. El primer equipo al que se le atribuye el avance del balón mediante el dribbling fue jugado en Yale en 1897, y la concesión oficial para el dribbling, solo se permitía uno al principio, se adoptó cuatro años después. Otro movimiento importante del baloncesto, el slam dunk, fue prohibido justo antes de la temporada 1967-1968 hasta la temporada 1976-1977.
+4. Mientras más mejor
+El numero de jugadores por lado no estaba especificado. Naismith inventó una actividad invernal de interior y quería un juego lo suficientemente flexible como para incluir a quien quisiera jugar. Durante un tiempo, el número total de jugadores fue un valor predeterminado de 18, nueve por lado, el mismo número que apareció en el primer juego.
+5. Si no había herida, no había falta
+Nunca se permitía cargar con el hombro, sujetar, empujar, hacer tropezar o golpear a un oponente. Sin embargo, tales infracciones nunca se consideraron faltas hasta 1910, con el advenimiento de una regla que descalificaban a un jugador por cometer cuatro de ellas. Ese total se elevó a cinco en 1946, en las reglas inaugurales de la Asociación de Baloncesto de América (el nombre original de la Asociación Nacional de Baloncesto), ya seis el año siguiente.
+6. Los árbitros usaban relojes
+Eso se debe a que uno de los deberes oficiales de los primeros árbitros era el cronometraje. Por otra parte, no había tanto tiempo para mantener: el reloj de tiro de 24 segundos no se instituyó hasta 1954, para combatir las tácticas de estancamiento que los equipos de la NBA habían comenzado a emplear.
+</button>
 
-        <button id="close-image"><img src="NFS.png" alt="Need For Speed" width="500" height="500"></img> Need For Speed</button>
+        <button id="close-image"><img src="NFS.png" alt="Need For Speed" width="500" height="500"></img>El 29 de enero de 1886, Carl Benz patentó su “vehículo motorizado con motor de gasolina” de tres ruedas, y se consideró como el día del nacimiento del automóvil. Ese mismo año, Gottlieb Daimler creó el primer carruaje motorizado de cuatro ruedas.
 
-        <button id="close-image"><img src="bloodborne.png" alt="Dark souls bloodborne" width="500" height="500"></img>Bloodborne</button>
+En 1886 comenzó la exitosa y larga historia de Mercedes-Benz que ya tiene 132 años de vida y sigue revolucionando el mundo automotriz. El certificado de nacimiento del automóvil lleva el número DRP 37435 y registró el invento del “vehículo motorizado con motor de gasolina” presentado por el ingeniero Carl Benz el 29 de enero 1886 en la Oficina Alemana de Patentes Imperial en Berlín. El documento de 132 años de edad, es un testimonio de espíritu innovador de Carl Benz, su poder creativo y visión empresarial. Hace 9 años que el documento de patente forma parte del Programa Memoria del Mundo de la Unesco, Organización de Naciones Unidas para la Educación, la Ciencia y la Cultura.</button>
+
+        <button id="close-image"><img src="bloodborne.png" alt="Dark souls bloodborne" width="500" height="500"></img>El conflicto israelí-palestino es el conflicto social y armado en curso entre israelíes y palestinos por el control de la tierra de Palestina, que se remonta a principios del siglo xx.4​ El conflicto, de gran envergadura, forma parte importante del conflicto árabe-israelí.
+
+En respuesta al creciente antisemitismo que se vivía en Europa, a finales del siglo xix surgió un movimiento sionista que defendía el establecimiento de una Patria para el pueblo judío en Palestina. Muchos judíos emigraron allí desde entonces y masivamente al término de la Segunda Guerra Mundial, a causa del Holocausto Nazi.
+
+Debido al incremento masivo de población judía en Palestina, aparecieron brotes de violencia cada vez más intensa entre las poblaciones judías y árabes. Como intento de solución, Naciones Unidas aprobó la partición de lo que había sido el mandato británico en Palestina en dos: Una parte judía y una parte árabe.
+
+El Estado judío supondría un 55% del territorio del Mandato, incluido el desierto del Néguev, y su población estaría formada por 500.000 judíos y 400.000 árabes palestinos. En ese momento, los judíos solo poseían el 7% de las tierras de Palestina.
+
+El Estado árabe palestino tendría el 44% del territorio del Mandato y una población de 725.000 árabes palestinos con una minoría de unos 10.000 judíos.5​
+
+Los judíos aceptaron el plan mientras que los árabes lo rechazaron. Estalló entonces una guerra civil en el mandato entre judíos y palestinos que desencadenó la expulsión o huida de dos tercios de la población palestina. El 14 de mayo de 1948, coincidiendo con la declaración de independencia de Israel, los Estados árabes vecinos declararon la guerra al recién creado estado de Israel, aunque finalmente fueron derrotados por los israelíes. A la conclusión de la guerra, Israel se negó a aceptar el retorno de los más de 700.000 refugiados palestinos, que han vivido desde entonces en campamentos de refugiados y ciudades de Líbano, Siria, Jordania, la Franja de Gaza y Cisjordania, entre otros lugares.</button>
       </header>
     </div>
   );
